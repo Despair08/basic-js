30ec3bf70e4a800058e3e652670e013b352daa78
Despair08 <vodanila2008@ya.ru>
2023-04-15 14:23:04 +0300

    feat: repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  if(str === null){
+    str = 'null';
+  }
+
+  let arr = [];
+
+  
+  if (options.addition !== undefined) {
+    let addition = options.addition;
+    if(addition === null){
+      addition = 'null';
+    }
+    
+    if (!options.additionRepeatTimes) {
+      str += addition;
+    } else {
+      let addArr = [];
+
+      for (let i = 0; i < options.additionRepeatTimes; i++) {
+        addArr.push(addition)
+      }
+      if (options.additionSeparator) {
+        addition = addArr.join(`${options.additionSeparator}`);
+      }else{
+        addition = addArr.join('|');
+      }
+
+      str += addition;
+
+    }
+  }
+
+  if (!options.repeatTimes) {
+    arr.push(str);
+  } else {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      arr.push(str);
+    }
+  }
+
+  options.separator ? result = arr.join(`${options.separator}`) : result = arr.join('+')
+
+  return result;
 }
 
 module.exports = {
--- src/temp.js
+++ src/temp.js
@@ -1,11 +1,43 @@
-calculateDepth(arr) {
-  let counter = 1;
-  let max = 1;
+let str = true;
+let options = { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' };
+// 'truefalse!!!false??? truefalse!!!false??? truefalse!!!false'
 
-  for (let elem of arr) {
-    if (Array.isArray(elem)) {
-      max = Math.max(this.calculateDepth(elem), max);
+function repeater(str, options) {
+
+  str = str.toString();
+
+  let arr = [];
+  let addition = options.addition.toString();
+
+  if (addition) {
+
+    if (!options.additionRepeatTimes) {
+      str += addition;
+    } else {
+      if (options.additionSeparator) {
+        let addArr = [];
+
+        for (let i = 0; i < options.additionRepeatTimes; i++) {
+          addArr.push(addition)
+        }
+        addition = addArr.join(`${options.additionSeparator}`);
+      }
+      str += addition;
+
     }
   }
-  return counter + max;
+
+  if (!options.repeatTimes) {
+    arr.push(str);
+  } else {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      arr.push(str);
+    }
+  }
+
+  options.separator ? result = arr.join(`${options.separator}`) : result = arr.join('+')
+
+  return result;
 }
+
+console.log(repeater(str, options));
